5dad4de7a63aa3b96bb7ab30a9e01cc7cdd0265f
Maxime Jeannin <maxime.jeannin@hotmail.fr>
2025-06-16 20:44:47 +0200

    feat: ajout de Spring Security et des méthodes associées pour gérer les accès aux pages

--- src/main/java/fr/eni/tp/filmotheque/bll/WebSecurityConfig.java
+++ src/main/java/fr/eni/tp/filmotheque/bll/WebSecurityConfig.java
@@ -2,11 +2,50 @@ package fr.eni.tp.filmotheque.bll;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 
 
 public class WebSecurityConfig {
 
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/index").permitAll()
+                        .requestMatchers("/", "/films/liste-films").permitAll()
+                        .requestMatchers("/", "/films/detail").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 
 }
