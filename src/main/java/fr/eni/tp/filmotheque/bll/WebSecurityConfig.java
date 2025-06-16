542f8ffe2ef440b5cdbda1fcbe3162e0459bd8cf
Maxime Jeannin <maxime.jeannin@hotmail.fr>
2025-06-16 17:09:48 +0200

    megacommit d'enfoiré

---
+++ src/main/java/fr/eni/tp/filmotheque/bll/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package fr.eni.tp.filmotheque.bll;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+
+public class WebSecurityConfig {
+
+
+}
--- src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
+++ src/main/java/fr/eni/tp/filmotheque/controller/FilmController.java
@@ -1,23 +1,44 @@
 package fr.eni.tp.filmotheque.controller;
 
 import fr.eni.tp.filmotheque.bll.FilmService;
+import fr.eni.tp.filmotheque.bo.Film;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequestMapping("films")
 
 @Controller
 public class FilmController {
 
     private FilmService filmService;
 
+    @Autowired
     public FilmController(FilmService filmService){
         this.filmService = filmService;
     }
 
-    public void afficherUnFilm(int id){
-        System.out.println(filmService.consulterFilmParId(id));
+    @RequestMapping(path="/liste-films",  method = {RequestMethod.GET, RequestMethod.POST})
+        private String listerFilms(Model filmListModel){
+        List<Film> listeFilms = filmService.consulterFilms();
+        filmListModel.addAttribute("listeFilms", listeFilms);
+        return "liste-films";
+    }
 
+    @RequestMapping(path="/detail",  method = {RequestMethod.GET, RequestMethod.POST})
+    private String detail(@RequestParam(defaultValue = "0") int id, Model filmListModel){
+        Film requestedFilm= filmService.consulterFilmParId(id);// souci d'index quid émarre a 1
+        filmListModel.addAttribute("requestedFilm", requestedFilm);
+        System.out.println(requestedFilm);
+        return "detail";
     }
-    public void afficherFilms(){
-        System.out.println(filmService.consulterFilms());
-    }
+
+
+
 }
